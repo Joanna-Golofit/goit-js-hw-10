e76a2c24b05fd6875aed87e3e838bd1c90bba7c5
Joanna-Golofit <sunderlajsa@gmail.com>
2021-10-01 14:15:51 +0200

    export i import { fetchCountries };

---
+++ src/fetchCountries.js
@@ -0,0 +1,8 @@
+// Napisz funkcję fetchCountries(name) która tworzy żądanie HTTP na nazwa źródła i przekazuje obietnicę z tablicą krajów - wynikiem żądania. Przenieś ją do oddzielnego pliku fetchCountries.js i utwórz eksport nazwany.
+
+
+const fetchCountries = name => {
+  console.log(name);
+}
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+import { fetchCountries } from "./fetchCountries";
+
+fetchCountries("asia");
+console.log("cos");
